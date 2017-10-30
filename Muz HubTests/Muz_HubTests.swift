e5fe4c4390c0529d85eae46f00a35cb1e0d7472f
Aqib Shehri <AqibMBP@Aqibs-MacBook-Pro.local>
2017-10-29 20:46:01 -0400

    Initial Commit

---
+++ Muz HubTests/Muz_HubTests.swift
@@ -0,0 +1,36 @@
+//
+//  Muz_HubTests.swift
+//  Muz HubTests
+//
+//  Created by Aqib Shehri on 10/29/17.
+//  Copyright © 2017 Aqib Shehri. All rights reserved.
+//
+
+import XCTest
+@testable import Muz_Hub
+
+class Muz_HubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
